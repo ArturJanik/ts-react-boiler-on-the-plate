ca577c00069d4fd92ecdf6e6e94f470d6a4d52bc
Artur Janik <arturjanik@MacBook-Pro-Artur.local>
2022-08-15 19:41:31 +0200

    COMMIT-1 | Initial react boilerplate without cra

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from './components/Header/Header';
+
+export const App = (): JSX.Element => {
+    return (
+        <>
+            <Header />
+            <h1>
+                Welcome to React App thats build using Webpack and Babel separately
+            </h1>
+        </>
+    )
+};
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development", 
+    entry: "./src/index.js", 
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "main.js"
+    },
+    target: "web",
+    devServer: {
+        port: "9500",
+        static: ["./public"],
+        open: true,
+        hot: true,
+        liveReload: true
+    },
+    resolve: {
+        extensions: ['.js','.jsx','.ts','.tsx'] 
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
